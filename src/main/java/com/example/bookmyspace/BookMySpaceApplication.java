c7cdf63123fe48402563c5c284541e5887669577
Sellvakumar S S <sellvakumarss@Sellvakumars-MacBook-Pro.local>
2025-03-29 19:50:16 +0530

    Initial commit

---
+++ src/main/java/com/example/bookmyspace/BookMySpaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmyspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMySpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMySpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookmyspace/BookMySpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmyspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMySpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
